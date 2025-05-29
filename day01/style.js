2c73295298337856be453acaf9ca64ec1471d148
seulgi3927 <seulgi3927@gmail.com>
2025-05-29 14:11:45 +0900

    5/29 수업

--- day01/style.js
+++ day01/style.js
@@ -22,19 +22,25 @@ if ($screen && $toggleButton) {
 
 $toggleButton.addEventListener('click', () => {
     stopWatchOn = !stopWatchOn;
-    toggleButton.innerHTML = stopWatchOn ? '■' : '▶';
-    toggleButton.style.backgroundColor = stopWatchOn ? 'tomato' : 'steelblue';
+    $toggleButton.innerHTML = stopWatchOn ? '■' : '▶'; // 삼항식
+    $toggleButton.style.backgroundColor = stopWatchOn ? 'tomato' : 'steelblue';
     if (!stopWatchOn) {
+        // 첫번째 인자: 콜백함수
+        // 두번째 인자: Number
         clearInterval(timeInterval);
-        seconds = 0;
-        screen.innerHTML = '00:00:00';
+        /* 
+        1초 > 100
+        1분 > 60초 > 6000 
+        */
+        seconds = 0
+        $screen.innerHTML = '00:00:00';
     } else {
         timeInterval = setInterval( () => {
-            seconds++;
-            const mm = String(Math.floor(seconds));
-            const ss = String(Math.floor(seconds));
-            const cs = String(seconds % 100).padStart
-            screen.innerHTML = `${mm}:${ss}:${cs}`;
+            seconds++
+            const mm = String(Math.floor(seconds / 6000) % 60).padStart(2, '0');
+            const ss = String(Math.floor(seconds / 100) % 60).padStart(2, '0');
+            const cs = String(seconds % 100).padStart(2, '0')
+            $screen.innerHTML = `${mm}:${ss}:${cs}`
         }, 10);
     }
 })
